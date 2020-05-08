8384195c1c9d3ea39d526e158e3cdf96b86110bd
Tim Langner <tim.langner0@gmail.com>
2020-05-08 20:06:53 +0200

    :card_file_box: Add default scope to user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,50 +1,58 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define('users', {
-        Name: {
-            type: Sequelize.STRING,
-            allowNull: false,
+    const User = sequelize.define(
+        'users',
+        {
+            Name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            Email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            Coins: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 50,
+            },
+            Level: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 10,
+            },
+            Xp: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            Color: {
+                type: Sequelize.STRING,
+            },
+            HasColor: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            HasRainbow: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            Role: {
+                type: Sequelize.STRING,
+            },
+            IsChatBanned: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
         },
-        Email: {
-            type: Sequelize.STRING,
-            allowNull: false,
+        {
+            defaultScope: {
+                attributes: { exclude: ['createdAt', 'updatedAt'] },
+            },
         },
-        Coins: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 50,
-        },
-        Level: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 10,
-        },
-        Xp: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-        Color: {
-            type: Sequelize.STRING,
-        },
-        HasColor: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-        HasRainbow: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-        Role: {
-            type: Sequelize.STRING,
-        },
-        IsChatBanned: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: 0,
-        },
-    });
+    );
 
     return User;
 };
