90616dd6c6d2fa913e13b2364d6e577e3c81cd0d
Tim <tim.langner@tobit.software>
2020-05-12 18:12:05 +0200

    :construction: Begin inner join query to get user skins

--- app/controllers/skin.controller.js
+++ app/controllers/skin.controller.js
@@ -1,5 +1,7 @@
 const db = require('../models');
 const Skin = db.skin;
+const HasSkin = db.hasSkin;
+const User = db.user;
 const Op = db.Sequelize.Op;
 
 // Retrieve all Skins from the database.
@@ -13,6 +15,27 @@ exports.findAll = (req, res) => {
                 message:
                     err.message ||
                     'Some error occurred while retrieving all skins.',
+            });
+        });
+};
+
+// Find a single User with an id
+exports.findSkins = (req, res) => {
+    const id = req.params.id;
+
+    Skin.hasMany(HasSkin, {
+        foreignKey: 'SkinId',
+    });
+    HasSkin.belongsTo(User, { foreignKey: 'UserId' });
+    HasSkin.belongsTo(Skin, { foreignKey: 'SkinId' });
+    Skin.findAll({ where: { UserId: 4 }, include: [User, HasSkin]})
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send({
+                message: 'Error retrieving all Skins from User with id=' + id,
             });
         });
 };
