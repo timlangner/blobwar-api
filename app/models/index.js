1ba93a93240de5149adefc7267370bfa35bfcac1
Tim <tim.langner@tobit.software>
2020-05-12 18:11:20 +0200

    :wrench: Add hasSkin model

--- app/models/index.js
+++ app/models/index.js
@@ -21,5 +21,6 @@ db.sequelize = sequelize;
 
 db.user = require('./user.model.js')(sequelize, Sequelize);
 db.skin = require('./skin.model.js')(sequelize, Sequelize);
+db.hasSkin = require('./hasSkin.model.js')(sequelize, Sequelize);
 
 module.exports = db;
