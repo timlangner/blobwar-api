730810ca5a6ba6377d3ea3165f8ed1286b9963ab
Tim <tim.langner@tobit.software>
2020-05-15 11:30:03 +0200

    :card_file_box: Add token model

--- app/models/index.js
+++ app/models/index.js
@@ -22,5 +22,6 @@ db.sequelize = sequelize;
 db.user = require('./user.model.js')(sequelize, Sequelize);
 db.skin = require('./skin.model.js')(sequelize, Sequelize);
 db.hasSkin = require('./hasSkin.model.js')(sequelize, Sequelize);
+db.discordTokens = require('./discordTokens.model.js')(sequelize, Sequelize);
 
 module.exports = db;
