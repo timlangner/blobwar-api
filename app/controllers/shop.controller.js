378a7211c7b551d30c58f3e07ac9d82d6f5f993d
Tim <tim.langner0@gmail.com>
2020-05-22 22:59:25 +0200

    :sparkles: Filter skins with owned skins

--- app/controllers/shop.controller.js
+++ app/controllers/shop.controller.js
@@ -17,77 +17,144 @@ exports.findPremium = (req, res) => {
             {
                 model: HasSkin,
                 attributes: [],
-                where: { UserId: req.body.UserId },
+                where: { UserId: req.params.id },
                 required: true,
             },
         ],
     })
         .then((ownedUserSkins) => {
-            console.log('ownedUserSkins');
-            console.log(ownedUserSkins[0].dataValues.Id);    
-            
-            Skin.findAll({
-                attributes: ['Id', 'Price', 'Name', 'Xp'],
-                where: {
-                    Price: {
-                        [Op.gt]: 0,
+            const premiumSkinIds = [];
+            ownedUserSkins.forEach((id) => {
+                premiumSkinIds.push(id.dataValues.Id);
+            });
+            if (premiumSkinIds && premiumSkinIds.length > 0) {
+                Skin.findAll({
+                    attributes: ['Id', 'Price', 'Name', 'Xp'],
+                    where: {
+                        Price: {
+                            [Op.gt]: 0,
+                        },
+                        Xp: 0,
+                        Private: 0,
+                        [Op.not]: [{ Id: { [Op.in]: premiumSkinIds } }],
                     },
-                    Private: 0,
-                    Xp: 0,
-                    [Op.not]: [{ Id: [ownedUserSkins[0].dataValues.Id] }],
-                },
-            })
-                .then((premiumSkins => {
-            console.log(premiumSkins); 
-                    
+                }).then((premiumSkins) => {
                     res.send(premiumSkins);
-                }));
+                });
+            } else {
+                res.status(204).send({
+                    message: 'The user already owns all premium skins',
+                });
+            }
         })
         .catch((err) => {
             console.log(err);
             res.status(500).send({
                 message:
                     'Error retrieving all premium skins for User with the Id ' +
-                    req.body.UserId,
+                    req.params.id,
             });
         });
 };
 
 // Retrieve all free Skins from the database.
 exports.findFree = (req, res) => {
+    Skin.hasMany(HasSkin);
+    Skin.hasMany(User);
+    HasSkin.belongsTo(User, { targetKey: 'Id', foreignKey: 'UserId' });
+    HasSkin.belongsTo(Skin, { targetKey: 'Id', foreignKey: 'SkinId' });
     Skin.findAll({
-        where: { Price: 0, Xp: 0, Private: 0 }
+        attributes: ['Id'],
+        include: [
+            {
+                model: HasSkin,
+                attributes: [],
+                where: { UserId: req.params.id },
+                required: true,
+            },
+        ],
     })
-        .then((data) => {
-            res.send(data);
+        .then((ownedUserSkins) => {
+            const freeSkinIds = [];
+            ownedUserSkins.forEach((id) => {
+                freeSkinIds.push(id.dataValues.Id);
+            });
+            if (freeSkinIds && freeSkinIds.length > 0) {
+                Skin.findAll({
+                    attributes: ['Id', 'Price', 'Name', 'Xp'],
+                    where: {
+                        Price: 0,
+                        Xp: 0,
+                        Private: 0,
+                        [Op.not]: [{ Id: { [Op.in]: freeSkinIds } }],
+                    },
+                }).then((freeSkins) => {
+                    res.send(freeSkins);
+                });
+            } else {
+                res.status(204).send({
+                    message: 'The user already owns all free skins',
+                });
+            }
         })
         .catch((err) => {
+            console.log(err);
             res.status(500).send({
                 message:
-                    err.message ||
-                    'Some error occurred while retrieving all skins.',
+                    'Error retrieving all free skins for User with the Id ' +
+                    req.params.id,
             });
         });
 };
 
 // Retrieve all level Skins from the database.
 exports.findLevel = (req, res) => {
+    Skin.hasMany(HasSkin);
+    Skin.hasMany(User);
+    HasSkin.belongsTo(User, { targetKey: 'Id', foreignKey: 'UserId' });
+    HasSkin.belongsTo(Skin, { targetKey: 'Id', foreignKey: 'SkinId' });
     Skin.findAll({
-        where: {
-            Xp: {
-                [Op.gt]: 0,
+        attributes: ['Id'],
+        include: [
+            {
+                model: HasSkin,
+                attributes: [],
+                where: { UserId: req.params.id },
+                required: true,
             },
-            Private: 0
-        },
+        ],
     })
-        .then((data) => {
-            res.send(data);
+        .then((ownedUserSkins) => {
+            const levelSkinIds = [];
+            ownedUserSkins.forEach((id) => {
+                levelSkinIds.push(id.dataValues.Id);
+            });
+            if (levelSkinIds && levelSkinIds.length > 0) {
+                Skin.findAll({
+                    attributes: ['Id', 'Price', 'Name', 'Xp'],
+                    where: {
+                        Price: 0,
+                        Xp: {
+                            [Op.gt]: 0,
+                        },
+                        Private: 0,
+                        [Op.not]: [{ Id: { [Op.in]: levelSkinIds } }],
+                    },
+                }).then((levelSkins) => {
+                    res.send(levelSkins);
+                });
+            } else {
+                res.status(204).send({
+                    message: 'The user already owns all level skins',
+                });
+            }
         })
         .catch((err) => {
+            console.log(err);
             res.status(500).send({
                 message:
-                    err.message ||
-                    'Some error occurred while retrieving all skins.',
+                    'Error retrieving all level skins for User with the Id ' +
+                    req.params.id,
             });
         });
 };
