3c0d3ec8af1f0b1e33c3756342dd66e129221cb0
Ronnie <rwaebers@gmail.com>
2020-05-24 02:49:13 +0200

    :bug: Default role is Guest

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, Sequelize) => {
             Role: {
                 type: Sequelize.STRING,
                 allowNull: false,
-                defaultValue: 'Player',
+                defaultValue: 'Guest',
             },
             Muted: {
                 type: Sequelize.INTEGER,
