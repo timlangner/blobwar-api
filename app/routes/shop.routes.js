f46de5467dd20859a4727615ad4e22705cc13581
Ronnie <rwaebers@gmail.com>
2020-05-26 23:25:56 +0200

    revert

--- app/routes/shop.routes.js
+++ app/routes/shop.routes.js
@@ -7,19 +7,19 @@ module.exports = (app) => {
     router.get('/skins/premium/:id', shop.findPremium);
 
     // POST request to buy premium skin
-    router.post('/skins/premium/:id', shop.buyPremium)
+    // router.post('/skins/premium/:id', shop.buyPremium)
 
     // Retrieve all free skins
     router.get('/skins/free/:id', shop.findFree);
 
     // POST request to claim free skin
-    router.post('/skins/free/:id', shop.claimFree)
+    // router.post('/skins/free/:id', shop.claimFree)
 
     // Retrieve all level skins
     router.get('/skins/level/:id', shop.findLevel);
 
     // POST request to claim level skin
-    router.post('/skins/level/:id', shop.claimLevel)
+    // router.post('/skins/level/:id', shop.claimLevel)
 
     // Retrieve all skins from a user
     router.get('/skins/owned/:id', shop.findOwned);
