2a1d021d33d3868e14d123f1849166f14535e936
Tim <tim.langner0@gmail.com>
2020-06-01 16:51:41 +0200

    :art: Renaming model

--- app/models/index.js
+++ app/models/index.js
@@ -24,6 +24,6 @@ db.skin = require('./skin.model.js')(sequelize, Sequelize);
 db.hasSkin = require('./hasSkin.model.js')(sequelize, Sequelize);
 db.badWord = require('./badWord.model.js')(sequelize, Sequelize);
 db.boostPlan = require('./boostPlan.model.js')(sequelize, Sequelize);
-db.boostHistory = require('./boostHistory.model.js')(sequelize, Sequelize);
+db.currentBoost = require('./currentBoost.model.js')(sequelize, Sequelize);
 
 module.exports = db;
