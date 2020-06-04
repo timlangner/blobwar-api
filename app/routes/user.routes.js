56c62be170c38f720663bd3ef9298a58eb3d6010
Tim <tim.langner@tobit.com>
2020-06-04 11:51:29 +0200

    :lock: Add authorization

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,28 +5,22 @@ module.exports = (app) => {
 
     var router = require('express').Router();
 
-    // // Authorization Middleware init
-    // const checkJwt = jwt({
-    //     secret: jwksRsa.expressJwtSecret({
-    //         cache: true,
-    //         rateLimit: true,
-    //         jwksRequestsPerMinute: 5,
-    //         jwksUri: `https://dev-vlkok0uj.eu.auth0.com/.well-known/jwks.json`,
-    //     }),
+    // Authorization Middleware init
+    const checkJwt = jwt({
+        secret: jwksRsa.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `https://dev-vlkok0uj.eu.auth0.com/.well-known/jwks.json`,
+        }),
 
-    //     // Validate the audience and the issuer.
-    //     audience: 'https:/blobwar.io/api/v1/',
-    //     issuer: `https://dev-vlkok0uj.eu.auth0.com/`,
-    //     algorithms: ['RS256'],
-    // });
+        // Validate the audience and the issuer.
+        audience: 'https:/blobwar.io/api/v1/',
+        issuer: `https://dev-vlkok0uj.eu.auth0.com/`,
+        algorithms: ['RS256'],
+    });
 
-    // // Check for Authorization
-    // router.use(checkJwt);
-
-    // Retrieve all Users
-    router.get('/', user.findAll);
-
-    // Create a new User
+        // Create a new User
     router.post('/', user.create);
 
     // Create a new User
@@ -34,6 +28,12 @@ module.exports = (app) => {
 
     // Retrieve the top 100 users with the most xp
     router.get('/leaderboard', user.getLeaderBoard);
+
+    // Check for Authorization for below routes
+    router.use(checkJwt);
+
+    // Retrieve all Users
+    router.get('/', user.findAll);
 
     // Retrieve a single User with id
     router.get('/:id', user.findOne);
