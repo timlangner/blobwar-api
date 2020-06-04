7e23c6da862f8cdb341dbaeb24eca8cb186db318
Ronnie <rwaebers@gmail.com>
2020-06-04 19:26:56 +0200

    :bug: fix recaptcha

--- app/controllers/recaptcha.controller.js
+++ app/controllers/recaptcha.controller.js
@@ -1,24 +1,17 @@
-const request = require('request');
-
 exports.verify = (req, res) => {
     const token = req.body.Token;
+    const secret = '6LfwO_UUAAAAACRYeMr8rc0aGMxzylogb9MP0JjD';
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
 
-    const verifyBody = {
-        secret: '6LfwO_UUAAAAACRYeMr8rc0aGMxzylogb9MP0JjD',
-        response: token
-    }
 
-    request({
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        url: 'https://www.google.com/recaptcha/api/siteverify',
-        body: JSON.stringify(verifyBody)
-    }, (err, response) => {
-        if (err) {
+    fetch(url, {
+        method: 'post'
+    }).then(response => response.json())
+        .then(data => {
+            console.log(data)
+        }).catch(() => {
             res.status(500).send({
                 message: "Error occured whilst trying to validate recaptcha."
             })
-        } else {
-            console.log(response.body)
-        }
-    })
+        })
 }
